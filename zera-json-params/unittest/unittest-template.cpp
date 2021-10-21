2afe0ef771a4a0c29e6809ba74b972abc72df9f7
Andreas Müller <schnitzeltony@gmail.com>
2021-10-21 19:59:00 +0200

    Unittest rework
    * Create json files for test cases
    * Do tests more systematic - the initial tests did not cover combinations systematically
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

---
+++ zera-json-params/unittest/unittest-template.cpp
@@ -0,0 +1,105 @@
+#include "gtest/gtest.h"
+
+#include <QJsonDocument>
+#include "zerajsonhelper.h"
+
+#include "zera-json-params-structure.h"
+
+// negative: empty structure (is not a template test exactly...)
+TEST(TEST_TEMPLATE,EMPTY){
+    QJsonObject jsonStructureRaw;
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.length(), 1) << "Expect one error for empty structure";
+    for(auto err : errListStructure) {
+        EXPECT_EQ(err.m_errType, cZeraJsonParamsStructure::errorTypes::ERR_INVALID_PARAM_DEFINITION) << "Expect ERR_INVALID_PARAM_DEFINITION";
+    }
+}
+
+// positive: valid template
+TEST(TEST_TEMPLATE,VALID){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.empty(), true) << "Expect no errors";
+    QJsonObject jsonStructure = jsonParamStructure.jsonStructure();
+
+}
+
+// positive: valid template / resolve one parameter
+TEST(TEST_TEMPLATE,VALID_CONTENT){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_CONTENT");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.empty(), true) << "Expect no errors";
+    QJsonObject jsonStructure = jsonParamStructure.jsonStructure();
+    int elemCount = jsonStructure.count();
+    // Important note: While debugging this the param 'param_from_template' is displayed as 'param_fro'!!!
+    // But our test below passes
+    EXPECT_EQ(elemCount, 1) << "Expect one entry 'params";
+    if(elemCount == 1) {
+        QJsonObject::const_iterator iterOnFirst = jsonStructure.begin();
+        bool isObject = iterOnFirst.value().isObject();
+        EXPECT_EQ(isObject, true) << "Expect 'params' a json object";
+        if(isObject) {
+            QJsonObject subObj = iterOnFirst.value().toObject();
+            int subElemCount = subObj.count();
+            EXPECT_EQ(subElemCount, 1) << "Expect one sub entry 'param_from_template";
+            if(elemCount == 1) {
+                QJsonObject::const_iterator itersubObj = subObj.begin();
+                EXPECT_EQ(itersubObj.key(), "param_from_template") << "Expect 'param_from_template' but found" << qPrintable(itersubObj.key());
+            }
+        }
+    }
+    // Check whole content
+    QJsonObject jsonStructureExpected = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_CONTENT_RESOLVE_EXPECTED");
+    EXPECT_EQ(jsonStructureExpected, jsonStructure) << "Expected resolved structuture does not match";
+}
+
+// positive: valid template / resolve one parameter
+TEST(TEST_TEMPLATE,VALID_CONTENT_NESTED){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_CONTENT_NESTED");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.empty(), true) << "Expect no errors";
+    QJsonObject jsonStructure = jsonParamStructure.jsonStructure();
+    // Check whole content
+    QJsonObject jsonStructureExpected = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_CONTENT_NESTED_EXPECTED");
+    EXPECT_EQ(jsonStructureExpected, jsonStructure) << "Expected resolved nested structuture does not match";
+}
+
+// positive: valid template / crete and check default
+TEST(TEST_TEMPLATE,VALID_ALL_TYPES){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_ALL_TYPES");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.empty(), true) << "Expect no errors";
+    QJsonObject jsonParamDefault = jsonParamStructure.createDefaultJsonState();
+    QJsonObject jsonParamDefaultExpected = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,VALID_ALL_TYPES_DEFAULT");
+    EXPECT_EQ(jsonParamDefaultExpected, jsonParamDefaultExpected) << "Expected default params do not match";
+}
+
+// negative: check extraction of template indirectly by invalid parameter: min > max & default > max
+TEST(TEST_TEMPLATE,MIN_LARGER_MAX){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,MIN_LARGER_MAX");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.length(),2);
+    for(auto err : errListStructure){
+        EXPECT_EQ(err.m_errType,cZeraJsonParamsStructure::errorTypes::ERR_INVALID_PARAM_TEMPLATE_DEFINITION);
+    }
+}
+
+// negative: check extraction of template indirectly by invalid parameter: type
+TEST(TEST_TEMPLATE,INVALID_TYPE){
+    QJsonObject jsonStructureRaw = ZeraJsonHelper::loadFromQrc("TEST_TEMPLATE,INVALID_TYPE");
+    cZeraJsonParamsStructure jsonParamStructure;
+    cZeraJsonParamsStructure::ErrList errListStructure = jsonParamStructure.loadStructure(jsonStructureRaw);
+    EXPECT_EQ(errListStructure.length(),1);
+    for(auto err : errListStructure){
+        EXPECT_EQ(err.m_errType,cZeraJsonParamsStructure::errorTypes::ERR_INVALID_PARAM_TEMPLATE_DEFINITION);
+    }
+}
+
+// we are not testing all combinations of incorrect parameter definitions here: it is done at parameter test
+
--- zera-json-params/unittest/zerajsonhelper.cpp
+++ zera-json-params/unittest/zerajsonhelper.cpp
@@ -1,9 +1,22 @@
 #include "zerajsonhelper.h"
 #include <iostream>
+#include <QFile>
+#include <QJsonDocument>
 
 ZeraJsonHelper::ZeraJsonHelper()
 {
+}
 
+QJsonObject ZeraJsonHelper::loadFromQrc(const QString strTestName)
+{
+    QJsonObject objRet;
+    QString fileName = QStringLiteral("://json-test-files/")+strTestName+QStringLiteral(".json");
+    QFile jsonFile(fileName);
+    if(jsonFile.open(QIODevice::Unbuffered | QIODevice::ReadOnly)) {
+        objRet = QJsonDocument::fromJson(jsonFile.readAll()).object();
+    }
+    checkJsonObject(objRet);
+    return objRet;
 }
 
 bool ZeraJsonHelper::checkJsonObject(QJsonObject p_data)
@@ -11,57 +24,57 @@ bool ZeraJsonHelper::checkJsonObject(QJsonObject p_data)
     bool retVal=false;
     if(p_data.empty()){
         retVal=true;
-        std::cerr << "The Provided json object is empty. Your test might fail Therefore" << std::endl;
+        std::cerr << "The Provided json object is empty. Your test might fail therefore" << std::endl;
     }
     return retVal;
 }
 
+
+// These following methods can go - keep them as reminder for now
 QJsonObject ZeraJsonHelper::manipulateJson(const QJsonObject &p_base, QStringList p_keyList, const QJsonValue &p_newValue)
 {
-    QString finalKey=p_keyList.last();
-    p_keyList.pop_back();
+    QString finalKey=p_keyList.takeLast();
     QJsonObject retVal=p_base;
     QMap<QString,QJsonObject> objectArray;
     QJsonObject lastObj=retVal;
-        for(QString key : p_keyList){
-            if(!lastObj.contains(key)){
-                lastObj.insert(key,QJsonObject());
-            }
-                lastObj=lastObj[key].toObject();
+    for(QString key : p_keyList){
+        if(!lastObj.contains(key)) {
+            lastObj.insert(key,QJsonObject());
         }
-        lastObj[finalKey]=p_newValue;
-        objectArray[p_keyList.last()]=lastObj;
-        for (QStringList::reverse_iterator i = p_keyList.rbegin();i != p_keyList.rend(); ++i ){
-            QJsonObject buf=objectArray[*i+1];
-            buf[*i]=lastObj;
-            lastObj=buf;
-            objectArray[*i]=buf;
-        }
+        lastObj=lastObj[key].toObject();
+    }
+    lastObj[finalKey]=p_newValue;
+    objectArray[p_keyList.last()]=lastObj;
+    for (QStringList::reverse_iterator i = p_keyList.rbegin();i != p_keyList.rend(); ++i ){
+        QJsonObject buf=objectArray[*i+1];
+        buf[*i]=lastObj;
+        lastObj=buf;
+        objectArray[*i]=buf;
+    }
     retVal=lastObj;
     return retVal;
 }
 
 QJsonObject ZeraJsonHelper::removeJsonItem(const QJsonObject p_base, QStringList p_keyList)
 {
-    QString finalKey=p_keyList.last();
-    p_keyList.pop_back();
+    QString finalKey=p_keyList.takeLast();
     QJsonObject retVal=p_base;
     QMap<QString,QJsonObject> objectArray;
     QJsonObject lastObj=retVal;
-        for(QString key : p_keyList){
-            if(!lastObj.contains(key)){
-                lastObj.insert(key,QJsonObject());
-            }
-                lastObj=lastObj[key].toObject();
+    for(QString key : p_keyList) {
+        if(!lastObj.contains(key)) {
+            lastObj.insert(key,QJsonObject());
         }
-        lastObj.remove(finalKey);
-        objectArray[p_keyList.last()]=lastObj;
-        for (QStringList::reverse_iterator i = p_keyList.rbegin();i != p_keyList.rend(); ++i ){
-            QJsonObject buf=objectArray[*i+1];
-            buf[*i]=lastObj;
-            lastObj=buf;
-            objectArray[*i]=buf;
-        }
+        lastObj=lastObj[key].toObject();
+    }
+    lastObj.remove(finalKey);
+    objectArray[p_keyList.last()]=lastObj;
+    for (QStringList::reverse_iterator i = p_keyList.rbegin();i != p_keyList.rend(); ++i ) {
+        QJsonObject buf=objectArray[*i+1];
+        buf[*i]=lastObj;
+        lastObj=buf;
+        objectArray[*i]=buf;
+    }
     retVal=lastObj;
     return retVal;
 }
