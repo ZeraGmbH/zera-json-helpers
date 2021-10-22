466ec18dd5af3c94e47e338802b19ca6101ac537
Andreas Müller <schnitzeltony@gmail.com>
2021-10-22 15:38:59 +0200

    zera-json-qml: Add a test for unwanted int cast
    During tests we had to learn that numbers are casted to int on toJson(). So
    first write a test for it which is failing as expected.
    
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-json-qml/tests/unittest-cpp.cpp
+++ zera-json-qml/tests/unittest-cpp.cpp
@@ -12,3 +12,16 @@ TEST(TEST_CPP,TYPE_LOAD_UNLOAD_COMPARE) {
     QJsonObject jsonReturnedData = declarativeJsonItem.toJson();
     EXPECT_EQ(jsonOrigData, jsonReturnedData);
 }
+
+// positive: check if double is not casted to int (by QVariant magic)
+TEST(TEST_CPP, NO_UNWANTED_INT_CAST) {
+    QJsonObject jsonObj;
+    QString strKey = "testInt";
+    jsonObj.insert(strKey, 1);
+    DeclarativeJsonItem declarativeJsonItem;
+    declarativeJsonItem.fromJson(jsonObj);
+    double newValue = 1.001;
+    declarativeJsonItem[strKey] = newValue;
+    QJsonObject jsonReturnedData = declarativeJsonItem.toJson();
+    EXPECT_EQ(jsonReturnedData[strKey], newValue);
+}
