38067c29a9a0ccc7e1a430c99ca894593bc6981b
bhamacher <b.hamacher@zera.de>
2021-12-09 09:38:18 +0100

    zera-json-params: Implement jsonstatefilepersistence functions
    Load state from file (Load default on error an fix file)
    Store state in file (Do not store in case of invalid state)
    
    Signed-off-by: bhamacher <b.hamacher@zera.de>

--- zera-json-params/src/jsonstatefilepersistence.cpp
+++ zera-json-params/src/jsonstatefilepersistence.cpp
@@ -1,5 +1,10 @@
 #include "jsonstatefilepersistence.h"
 
+#include "zera-jsonfileloader.h"
+#include "zera-json-params-state.h"
+
+#include <QFile>
+#include <QJsonDocument>
 
 JsonStateFilePersistence::JsonStateFilePersistence()
 {
@@ -8,20 +13,57 @@ JsonStateFilePersistence::JsonStateFilePersistence()
 
 void JsonStateFilePersistence::setStateFilePath(const QString& path)
 {
+    m_stateFilePath=path;
 }
+
 
 void JsonStateFilePersistence::setJsonParamStructure(const QJsonObject& jsonParamStructure)
 {
+    m_jsonParamStructure=jsonParamStructure;
 }
 
 QJsonObject JsonStateFilePersistence::getJsonParamStructure() const
 {
+    return m_jsonParamStructure;
+}
+
+bool JsonStateFilePersistence::checkStateValidity(const QJsonObject &stateObject)
+{
+    bool isValid=false;
+    cZeraJsonParamsState jsonParamsState;
+    jsonParamsState.setStructure(m_jsonParamStructure);
+    cZeraJsonParamsState::ErrList errList = jsonParamsState.validateJsonState(stateObject);
+    if(errList.isEmpty()){
+        isValid=true;
+    }
+    return isValid;
 }
 
 QJsonObject JsonStateFilePersistence::loadState()
 {
+    QJsonObject paramState;
+    cZeraJsonParamsState jsonParamsState;
+
+    try{
+        jsonParamsState.setStructure(m_jsonParamStructure);
+        QJsonObject stateObject = cJsonFileLoader::loadJsonFile(m_stateFilePath);
+        bool isValidState=checkStateValidity(stateObject);
+        if(!isValidState) {
+            throw std::runtime_error("invalid struct");
+        }
+        paramState = stateObject;
+
+    }catch(std::runtime_error &e){
+        paramState= jsonParamsState.createDefaultJsonState();
+        saveState(paramState);
+    }
+    return paramState;
 }
 
 void JsonStateFilePersistence::saveState(QJsonObject stateObject)
 {
+    bool isValidState=checkStateValidity(stateObject);
+    if(isValidState){
+        cJsonFileLoader::storeJsonFile(m_stateFilePath,stateObject);
+    }
 }
