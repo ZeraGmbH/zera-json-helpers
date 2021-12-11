5fc5cc7a18d725ba983306d045528136d29e14d8
Andreas Müller <schnitzeltony@gmail.com>
2021-12-11 18:52:42 +0100

    unittest-json-loader: Add test for cJsonFileLoader::storeJsonFile
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-json-params/unittest/unittest-json-loader.cpp
+++ zera-json-params/unittest/unittest-json-loader.cpp
@@ -1,6 +1,6 @@
-#include <gtest/gtest.h>
-
 #include "zera-jsonfileloader.h"
+#include <gtest/gtest.h>
+#include <QTemporaryFile>
 
 TEST(TEST_JSON_LOAD, LOAD_NOT_EXISTS) {
     QJsonObject json = cJsonFileLoader::loadJsonFile("foo");
@@ -13,4 +13,13 @@ TEST(TEST_JSON_LOAD, LOAD_EXISTING) {
     EXPECT_EQ(json["boolVal"], true);
     EXPECT_EQ(json["numVal"], 10);
     EXPECT_EQ(json["strVal"], "foo");
+}
+
+TEST(TEST_JSON_LOAD, STORE_AND_COMPARE) {
+    QJsonObject json = cJsonFileLoader::loadJsonFile(":/json-test-files/TEST_JSON_LOAD.json");
+    QTemporaryFile tempFile;
+    tempFile.open();
+    EXPECT_TRUE(cJsonFileLoader::storeJsonFile(tempFile.fileName(), json));
+    QJsonObject jsonStored = cJsonFileLoader::loadJsonFile(tempFile.fileName());
+    EXPECT_EQ(json, jsonStored);
 }
