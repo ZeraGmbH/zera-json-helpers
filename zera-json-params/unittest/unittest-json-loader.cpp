fe06b02aece3fbc5cacfc8bfbb776f4bd8586c9e
Andreas Müller <schnitzeltony@gmail.com>
2022-10-11 21:12:26 +0200

    unittest-json-loader: Check for contents on (in)valid json load
    Signed-off-by: Andreas Müller <schnitzeltony@gmail.com>

--- zera-json-params/unittest/unittest-json-loader.cpp
+++ zera-json-params/unittest/unittest-json-loader.cpp
@@ -28,16 +28,19 @@ TEST(TEST_JSON_LOAD, LOAD_NOT_EXIST_NO_PARSE_ERROR) {
     QJsonParseError jsonError = {};
     QJsonObject json = cJsonFileLoader::loadJsonFile("foo", &jsonError);
     EXPECT_EQ(jsonError.error, QJsonParseError::NoError);
+    EXPECT_TRUE(json.isEmpty());
 }
 
 TEST(TEST_JSON_LOAD, LOAD_EXISTING_NO_PARSE_ERROR) {
     QJsonParseError jsonError = {};
     QJsonObject json = cJsonFileLoader::loadJsonFile(":/json-test-files/TEST_JSON_LOAD.json", &jsonError);
     EXPECT_EQ(jsonError.error, QJsonParseError::NoError);
+    EXPECT_FALSE(json.isEmpty());
 }
 
 TEST(TEST_JSON_LOAD, LOAD_EXISTING_PARSE_ERROR) {
     QJsonParseError jsonError = {};
     QJsonObject json = cJsonFileLoader::loadJsonFile(":/json-test-invalid-files/TEST_JSON_LOAD_INVALID.json", &jsonError);
     EXPECT_NE(jsonError.error, QJsonParseError::NoError);
+    EXPECT_TRUE(json.isEmpty());
 }
